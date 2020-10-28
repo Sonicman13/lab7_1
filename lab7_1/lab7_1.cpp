a19bd7c30e172b943fcbdbf46b8de09a21e0a7c2
Danil <70899358+Sonicman13@users.noreply.github.com>
2020-10-28 06:20:05 +0300

    .

--- lab7_1/lab7_1.cpp
+++ lab7_1/lab7_1.cpp
@@ -235,11 +235,11 @@ int main()
     setlocale(LC_ALL, "RUS");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    class Store store1[10], * store2, * store3[10];
+    class Store store1[10], * store2;
     int amountDifference, numberOfItems, itemAmount[N], i, max, n, *g;
     double price, itemPrice[N];
     string s, s1[N], code, name, adress, itemCode[N], itemName[N], f;
-    printf("Работать с переменной, указателем или указателем на массив?(1 - переменная, 2 - указатель, все остальные символы - указатель на массив)\n");
+    printf("Работать с переменной, указателем или указателем на массив?(1 - переменная, 2 - указатель)\n");
     getline(cin, f);
 
     //Переменная
@@ -508,144 +508,6 @@ int main()
             }
         }
         delete[] store2;
-    }
-
-    //Массив указателей
-
-    else {
-        store3[0] = (class Store*)malloc(sizeof(Store));
-        s = "-";
-        s1[0] = "-";
-        printf("Использовать или read чтобы ввести данные(1 - read, все остальные символы - init)\n");
-        getline(cin, f);
-        if (f == "1") {
-            store3[0]->read();
-        }
-        else {
-            printf("\nВведите название магазина\n");
-            getline(cin, name);
-            printf("Введите адрес магазина\n");
-            getline(cin, adress);
-            numberOfItems = 0;;
-            printf("Добавить товар?(1-да,0-нет)\n");
-            getline(cin, f);
-            while (f == "1") {
-                printf("\nВведите название товара\n");
-                getline(cin, itemName[numberOfItems]);
-                printf("Введите код товара\n");
-                getline(cin, itemCode[numberOfItems]);
-                do {
-                    printf("Введите цену\n");
-                    scanf_s("%lf", &itemPrice[numberOfItems]);
-                } while (itemPrice[numberOfItems] < 0);
-                do {
-                    printf("Введите колличество товара\n");
-                    scanf_s("%d", &itemAmount[numberOfItems]);
-                } while (itemAmount[numberOfItems] < 0);
-                numberOfItems++;
-                printf("Добавить еще один товар?(1 - да, все остальные символы - нет)\n");
-                getline(cin, f);
-                getchar();
-            }
-            store3[0]->init(name, adress, numberOfItems, itemName, itemCode, itemPrice, itemAmount);
-        }
-        i = 0;
-        max = 1;
-        f = "1";
-        while (f != "11") {
-            store3[i]->displayName();
-            printf("\nВведите номер следующего действия:\n");
-            printf("1 - показать информацию о магазине\n");
-            printf("2 - добавить новый вид товара\n");
-            printf("3 - изменить цену товара\n");
-            printf("4 - изменить колличество товара\n");
-            printf("5 - Добавить магазин\n");
-            printf("6 - показать все магазины\n");
-            printf("7 - сменить магазин\n");
-            printf("8 - сложить магазины\n");
-            printf("9 - показать колличество товаров\n");
-            printf("10 - изменить колличество мест для товаров в магазине\n");
-            printf("11 - выйти\n");
-            getline(cin, f);
-            if (f == "1") {
-                store3[i]->display();
-            }
-            else if (f == "2") {
-                *store3[i] = *store3[i]++;
-            }
-            else if (f == "3") {
-                printf("\nВведите код товара\n");
-                getline(cin, code);
-                do {
-                    printf("Введите новую цену\n");
-                    scanf_s("%lf", &price);
-                } while (price < 0);
-                getchar();
-                store3[i]->priceChange(code, price);
-            }
-            else if (f == "4") {
-                printf("\nВведите код товара\n");
-                getline(cin, code);
-                printf("Введите на сколько изменилось колличество товара(если увеличилость - положительное число, если уменьшилось - отрицательное)\n");
-                scanf_s("%d", &amountDifference);
-                getchar();
-                store3[i]->amountChange(code, amountDifference);
-            }
-            else if (f == "5") {
-                store3[max] = (class Store*)malloc(sizeof(Store));
-                store3[max]->read();
-                i = max;
-                max++;
-
-            }
-            else if (f == "6") {
-                for (n = 0; n < max; n++) {
-                    store3[n]->displayName();
-                }
-            }
-            else if (f == "7") {
-                printf("\nВведите название магазина\n");
-                getline(cin, name);
-                for (n = 0; n < max; n++) {
-                    if (storecmp(name, *store3[n]) == 1) {
-                        i = n;
-                        n = max;
-                    }
-                }
-            }
-            else if (f == "8") {
-                printf("\nВведите название магазина\n");
-                getline(cin, name);
-                for (n = 0; n < max; n++) {
-                    if (storecmp(name, *store3[n]) == 1) {
-                        *store3[i] = *store3[i] + *store3[n];
-                        n = max;
-                    }
-                }
-            }
-            else if (f == "9") {
-                printf("\n1 - out, 0 - ref\n");
-                getline(cin, s);
-                if (s == "1")
-                {
-                    store3[i]->getNumber(g);
-                    printf("%d\n", *g);
-                }
-                else
-                {
-                    store3[i]->getNumber(&n);
-                    printf("%d\n", n);
-                }
-            }
-            else if (f == "10") {
-                printf("Введите колличество\n");
-                scanf_s("%d", &numberOfItems);
-                Store::maxNumberOfItemsChange(numberOfItems);
-            }
-        }
-        for (n = 0; n < max; n++) {
-            free(store3[n]);
-        }
     }
     return 0;
 }
